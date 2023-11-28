2aec9b1e65f00c721ce17332868934df6531df10
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-11-28 12:05:29 +0200

    feat: add layouts (Header, Footer, Container)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
-function App() {
-  return <>hello world</>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/utils/Container/Container.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ContainerWrap = styled.div`
+  padding: 0 20px;
+  margin: 0 auto;
+  width: 100%;
+  height: auto;
+
+  @media screen and (min-width: calc(768px - 0.02px)) {
+  }
+
+  @media screen and (min-width: calc(1280px - 0.02px)) {
+  }
+`;
